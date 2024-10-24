7092a53b2a6dded628366a674959732bc57f2b7c
Felix Allard <xilef992@gmail.com>
2024-10-24 16:13:57 -0400

    Initial Commit!

---
+++ README.md
@@ -0,0 +1,2 @@
+# VC-ChopZ
+This is the website for the barber shop VC ChopZ
---
+++ TestingProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace TestingProject;
+
+
+/// <summary>
+/// This is how you do test! Have fun!
+/// </summary>
+[TestFixture]
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

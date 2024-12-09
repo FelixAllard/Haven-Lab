057497554b98e9c622e7e93f6865096859c0fab5
Felix Allard <132389716+FelixAllard@users.noreply.github.com>
2024-12-09 04:52:08 -0500

    Finished Close to everything!

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,6 +10,7 @@ import Footer from "./Footer/Footer";
 import AboutUs from "./AboutUs/AboutUs";
 import Products from "./Pages/Products/Products";
 import Orders from "./Pages/Orders/Orders.js";
+import AddProductPage from "./Pages/Admin/Product/AddProductPage";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/orders" element={<Orders />} />
+              <Route path="/admin/product/create" element={<AddProductPage />} />
           </Routes>
         </div>
           <Footer />

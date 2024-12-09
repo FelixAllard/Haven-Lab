ae8d03a6cddd43f2b8ae33870689dca908dc22c1
Justin Morissette <123003289+Justin222993@users.noreply.github.com>
2024-12-09 13:04:59 -0500

    Playwright test wasn't sent in previous pull request

---
+++ PlaywrightTesting/tests/Update-product-page.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('UpdateOneProduct', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.getByLabel('Toggle navigation').click();
+    await page.getByRole('link', { name: 'Products' }).click();
+    await page.getByLabel('Close').click();
+    await page.locator('div:nth-child(3) > .card > .card-body > .btn > a').click();
+    await page.getByRole('button', { name: 'Update Product' }).click();
+    await page.locator('input[name="title"]').click();
+    await page.locator('input[name="title"]').fill('New Product!');
+    await page.getByRole('button', { name: 'Save Changes' }).click();
+    await expect(page.getByText('Product saved successfully!')).toBeVisible();
+    await page.getByText('Close').click();
+    await expect(page.getByRole('heading', { name: 'New Product!' })).toBeVisible();
+});

d6a37e39f17cb48ddbcd4da6e1e2767086104db6
AlexCristea04 <alexcristea004@gmail.com>
2025-01-19 21:15:12 -0500

    Promo shopify controller

---
+++ Api-Gateway/Services/ServicePromoController.cs
@@ -0,0 +1,47 @@
+﻿namespace Api_Gateway.Services;
+
+public class ServicePromoController
+{
+    private readonly IHttpClientFactory _httpClientFactory; 
+    private readonly string BASE_URL; 
+
+    public ServicePromoController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+        BASE_URL = Environment.GetEnvironmentVariable("BASE_URL_SHOPIFY_API")??"http://localhost:5106";
+    }
+    
+    public virtual async Task<string> GetAllPriceRulesAsync()
+    {
+        try
+        {
+            // Create the HttpClient instance using the factory
+            var client = _httpClientFactory.CreateClient(); // Uses default HttpClient configuration
+
+            var requestUrl = $"{BASE_URL}/api/pricerules"; // Shopify endpoint for orders
+
+            // Create the request message
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+
+            // Send the request and get the response
+            var response = await client.SendAsync(requestMessage);
+
+            if (response.IsSuccessStatusCode)
+            {
+                // Read and return the response content as string
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                // If the API call fails, return an error message
+                return $"Error fetching orders: {response.ReasonPhrase}";
+            }
+        }
+        catch (Exception ex)
+        {
+            // Return error details in case of an exception
+            return $"Exception: {ex.Message}";
+        } 
+    }
+
+}
